df00a64a091ec091cf26cd2affead6ba901c58e3
pro-sudhanshu <codsmasterg@gmail.com>
2024-10-29 17:44:51 +0530

    all production level setup is configured

--- src/app.js
+++ src/app.js
@@ -11,4 +11,10 @@ app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// routes imports
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
 export { app };
